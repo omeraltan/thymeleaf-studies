b876b69f604299e763b11f89b8cbd325251b0de7
omeraltan <omeraltan66@gmail.com>
2024-02-26 11:01:31 +0300

    Commit All

--- studies/src/main/java/com/thymeleaf/demo/controller/TaskController.java
+++ studies/src/main/java/com/thymeleaf/demo/controller/TaskController.java
@@ -1,15 +1,23 @@
 package com.thymeleaf.demo.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
 public class TaskController {
 
+    @Value("${countries}")
+    private List<String> countries;
+
     @GetMapping("/task")
     public String tasks(Model model){
+
+        model.addAttribute("countries",countries);
         return "/task";
     }
 
